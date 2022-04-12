92fef34760b8ba2199412e60f6197df57a765b05
Oliver Olsson <oliolss@student.chalmers.se>
2022-04-12 23:01:05 +0200

    update 12 april

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
+# from torchvision.transforms import ToTensor
 import numpy as np
 # from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 from matplotlib import style
-style.use("ggplot")
-from sklearn.model_selection import train_test_split
-
-import numpy as np
-# from torchvision.transforms import ToTensor
-import matplotlib.pyplot as plt
-from matplotlib import style
-style.use("ggplot")
 from sklearn.model_selection import train_test_split
 # from sklearn.metrics import accuracy_score
 from tqdm import tqdm
@@ -22,9 +15,12 @@ import os
 from tifffile import imread
 import time
 from sklearn.metrics import mean_squared_error
+style.use("ggplot")
 
 # Set to true if you want to build the data
 rebuild_data = False
+load_model = True
+
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
     print('Running on the GPU')
@@ -34,7 +30,7 @@ else:
 
 
 class Build_Dataset():
-    sarbilder = r'D:\Sar_download3'
+    sarbilder = r'D:\CNN_storage\Balanced_dataset_sep_2021_mini'
     count = 0
     training_data = []
 
@@ -46,8 +42,8 @@ class Build_Dataset():
                 pxlimg=np.array(img)
                 subpxlimg = blockshaped(pxlimg, 50, 50)     # Subdivide one 200x200 image into 16 50x50 images
                 p = f.split('.tif')[0]
-                label = float(p.split('_')[5])
-                for i in range(64):
+                label = float(p.split('_')[8])
+                for i in range(16):
 
                     self.training_data.append([subpxlimg[i], label])
                     self.count += 1
@@ -64,19 +60,19 @@ class Build_Dataset():
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 16, (5, 5))    # Conv layer
-        self.batchnorm1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, (5, 5))   # Conv layer 2
-        self.batchnorm2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 64, (5, 5))  # Conv layer 3
-        self.batchnorm3 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Conv2d(1, 32, (5, 5))    # Conv layer
+        self.batchnorm1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, (5, 5))   # Conv layer 2
+        self.batchnorm2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, (5, 5))  # Conv layer 3
+        self.batchnorm3 = nn.BatchNorm2d(128)
 
         # Intermediary part that finds the value of self._to_linear
         x = torch.randn(50, 50).view(-1, 1, 50, 50)
         self._to_linear = None
         self.convs(x)
-        self.fc1 = nn.Linear(self._to_linear, 512)  # Fully connected layer 1
-        self.fc2 = nn.Linear(512, 1)                # Fully connected layer 2
+        self.fc1 = nn.Linear(self._to_linear, 1080)  # Fully connected layer 1
+        self.fc2 = nn.Linear(1080, 1)                # Fully connected layer 2
 
     def convs(self, x):
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
@@ -109,40 +105,59 @@ def test(size=32):
     return val_loss
 
 
-def give_prediction(size=32):
-    random_start = np.random.randint(len(train_x)-size)
-    x, y = train_x[random_start:random_start+size], train_y[random_start:random_start+size]
-    x, y = x.view(-1, 1, 50, 50).to(device), y.to(device)
-    #x, y = train_x, train_y
-    #x, y = x.view(-1, 1, 50, 50).to(device), y.to(device)
-    outputs = net(x)
+def give_prediction(size=32, train_prediction=False):
+    if train_prediction:
+        random_start = np.random.randint(len(train_x) - size)
+        x, y = train_x[random_start:random_start + size], train_y[random_start:random_start + size]
+        x, y = x.view(-1, 1, 50, 50).to(device), y.to(device)
+        print("Prediction on the Training set")
+    else:
+        random_start = np.random.randint(len(test_x) - size)
+        x, y = test_x[random_start:random_start + size], test_y[random_start:random_start + size]
+        x, y = x.view(-1, 1, 50, 50).to(device), y.to(device)
+        print("Prediction on the validation set")
+    with torch.no_grad():
+        outputs = net(x)
     correct=0
+    correct1=0
+    correct2=0
     total=0
     for k in range(len(y)):
+        if abs(outputs.detach().cpu().numpy()[k]-y.detach().cpu().numpy()[k]) < 0.1:
+            correct += 1
+        if abs(outputs.detach().cpu().numpy()[k] - y.detach().cpu().numpy()[k]) < 0.2:
+            correct1 += 1
         if abs(outputs.detach().cpu().numpy()[k]-y.detach().cpu().numpy()[k]) < 0.5:
-            correct+=1
+            correct2 += 1
         total+=1
-    print("Accuracy:", round(correct / total, 3))
-    print("Total:", total)
+    print("Accuracy within 10 centimeters:", round(correct / total, 3))
+    print("Accuracy within 20 centmeters:", round(correct1 / total, 3))
+    print("Accuracy within 50 centimeters:", round(correct2 / total, 3))
 
     return outputs, y
 
 
 def train():
-    BATCH_SIZE = 228
-    EPOCHS = 1
+    BATCH_SIZE = 1000
+    EPOCHS = 5
     with open(f'{MODEL_NAME}.log', 'a') as f:
         for epoch in range(EPOCHS):
+            lossarray = []
+            vallossarray = []
             for i in tqdm(range(0, len(train_x), BATCH_SIZE)):
                 # print(i, i+BATCH_SIZE)
                 batch_x = train_x[i:i + BATCH_SIZE].view(-1, 1, 50, 50)
                 batch_y = train_y[i:i + BATCH_SIZE]
                 batch_x, batch_y = batch_x.to(device), batch_y.to(device)
                 loss = fwd_pass(batch_x, batch_y, train=True)
+                lossarray.append(loss.cpu().detach().numpy())
                 if i % BATCH_SIZE/2 == 0:
                     val_loss = test(size=BATCH_SIZE)
+                    vallossarray.append(val_loss.cpu().detach().numpy())
                     f.write(f'{MODEL_NAME}, {round(time.time(), 3)}, {round(float(loss), 4)}, {round(float(val_loss), 4)}\n')
-            print(f'Epoch: {epoch}. Loss: {loss}. Validation Loss: {val_loss}')
+            meanvalloss=np.mean(vallossarray)
+            meanloss=np.mean(lossarray)
+            print(f'Epoch: {epoch}. Loss: {meanloss}. Validation Loss: {meanvalloss}')
 
 
 def fwd_pass(x, y, train=False):
@@ -200,48 +215,77 @@ def create_loss_graph(model_name):
     ax2.plot(times, val_losses, label="val_loss")
     ax2.legend(loc=2)
     plt.show()
-#val_loss=test(size=8)
-#print(val_loss)
 
-# Only rebuilds the data if rebuild_data is true
-if rebuild_data:
-    make_data = Build_Dataset()
-    make_data.make_training_data()
 
-training_data = np.load('training_data.npy', allow_pickle=True)    # Load in pixelimages and labels
+def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
+    print("Saving Checkpoint")
+    torch.save(state, filename)
 
-# plt.imshow(training_data[0][0], cmap='gray')
-# plt.show()
 
-# Separates data into testing and training, both the images and the labels
+def load_checkpoint(checkpoint):
+    print("Loading checkpoint")
+    net.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
 
-x = torch.Tensor(np.array([i[0] for i in training_data])).view(-1, 50, 50)
-#x = (x - torch.mean(x))/torch.std(x)
-y = torch.Tensor(np.array([i[1] for i in training_data])).view(-1, 1)
 
-train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.1)
-print('training length: ', len(train_x))
-print('training label: ',len(train_y))
-print('test length: ',len(test_x))
-print('test label: ',len(test_y))
-# print(len(train_x))
-# print(len(test_x))
+def load_dataset():
+
+    # Only rebuilds the data if rebuild_data is true
+    if rebuild_data:
+        make_data = Build_Dataset()
+        make_data.make_training_data()
+
+    training_data = np.load('training_data.npy', allow_pickle=True)  # Load in pixel images and labels
+
+    # Plot the first SAR pixel image using the code below
+    # plt.imshow(training_data[0][0], cmap='gray')
+    # plt.show()
+
+    # Puts data and labels in torch format
+    x = torch.Tensor(np.array([i[0] for i in training_data])).view(-1, 50, 50)
+    # x = (x - torch.mean(x))/torch.std(x)
+    y = torch.Tensor(np.array([i[1] for i in training_data])).view(-1, 1)
+
+    # Splits the dataset into training and testing using a random seed
+    train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.1, random_state=42)
+    print('training length: ', len(train_x))
+    print('training label: ', len(train_y))
+    print('test length: ', len(test_x))
+    print('test label: ', len(test_y))
+    return train_x, test_x, train_y, test_y
 
 
 MODEL_NAME = f'model-{int(time.time())}'
+print("the model name is ", MODEL_NAME)
+
+train_x, test_x, train_y, test_y = load_dataset()
+
+# Generate an instance of the Neural network
 net = Net().to(device)
-optimizer = optim.Adam(net.parameters(), lr=0.00001)
+optimizer = optim.Adam(net.parameters(), lr=0.0001)
 loss_function = nn.MSELoss()
 
-print(MODEL_NAME)
-train()
-create_loss_graph(MODEL_NAME)
-output, input1 = give_prediction(1000)
+if load_model:
+    load_checkpoint(torch.load("my_checkpoint.pth.tar"))
+
+#train()  # Trains the model
+#create_loss_graph(MODEL_NAME)  # Generate loss graph
+
+checkpoint = {'state_dict': net.state_dict(), 'optimizer': optimizer.state_dict()}
+save_checkpoint(checkpoint)
+
+# Plot targets on x-axis and model prediction on y-axis
+output, input1 = give_prediction(1000, train_prediction=True)
 output = output.cpu().detach().numpy()
 input1 = input1.cpu().detach().numpy()
 
 plt.scatter(input1, output)
+plt.title("Prediction to target plot")
+plt.xlabel("Real significant wave height [m]")
+plt.ylabel("Predicted significant wave height [m]")
 plt.show()
 
-mse = mean_squared_error(input1, output)
-print(mse)
+# Calculates mean squared error manually
+# mse = mean_squared_error(input1, output)
+# print(mse)
+
--- test12.py
+++ test12.py
@@ -6,16 +6,29 @@ import pandas as pd
 import shutil
 
 #Folder where input data is stored
-path=os.chdir('D:\Sar_download3')
-lista = os.listdir('D:\Sardownload3')
+path=os.chdir('D:\CNN_storage\Balanced_dataset_sep_2021')
+lista = os.listdir('D:\CNN_storage\Balanced_dataset_sep_2021')
 
 waveheightarray = []
+wavearray = []
+aziarray = []
+meanarray = []
+vararray = []
 count=0
 for i in lista:
     q = i.split('.tif')[0]
-    p = q.split('')
+    p = q.split('_')
     try:
-        waveheight = int(10000000 * float(p[5]))
+        waveheight = int(10000000 * float(p[8]))
+
+        wave=float(p[8])
+        azimuth=float(p[7])
+        mean=float(p[5])
+        var=float(p[6])
+        wavearray.append(wave)
+        aziarray.append(azimuth)
+        meanarray.append(mean)
+        vararray.append(var)
         waveheightarray.append(waveheight)
     except Exception as e:
         print(str(e))
@@ -24,6 +37,25 @@ for i in lista:
 
 
 print('antal bilder som inte fungerade = ' + str(count))
+#wavearray = wavearray[0:20000]
+#vararray = vararray[0:20000]
+#aziarray = aziarray[0:20000]
+#meanarray = meanarray[0:20000]
+plt.scatter(wavearray, vararray)
+plt.title("Variance to waveheight relation")
+plt.xlabel("Significant waveheight [m]")
+plt.ylabel("Variance")
+plt.show()
+plt.scatter(wavearray, aziarray)
+plt.title("Azimuth cutoff to waveheight relation")
+plt.xlabel("Significant waveheight [m]")
+plt.ylabel("Azimuth cutoff wavelenght [m]")
+plt.show()
+plt.scatter(wavearray, meanarray)
+plt.title("Mean backscatter to waveheight relation")
+plt.xlabel("Significant waveheight [m]")
+plt.ylabel("Mean backscatter")
+plt.show()
 
 bins = np.linspace(0, 60000000, num=60)
 
@@ -38,37 +70,47 @@ print(Q[0])
 #b = np.asarray([2, 4])
 #c = np.split(d, b)
 
-sortedwaveheightarray = np.sort(waveheightarray)
-subdividedwaveheightarray = np.split(sortedwaveheightarray, np.cumsum(Q[0]))
-finalwaveheightarray = []
-count1=0
+#sortedwaveheightarray = np.sort(waveheightarray)
+#subdividedwaveheightarray = np.split(sortedwaveheightarray, np.cumsum(Q[0]))
+#finalwaveheightarray = []
+#count1=0
+#for j in range(0, 40):
+#    current = subdividedwaveheightarray[j]
+#    np.random.shuffle(current)
+#    if len(current) > 100:
+#        current = current[0:100]
+#
+#    for k in range(len(current)):
+#        finalwaveheightarray.append(current[k])
+#    print(np.size(finalwaveheightarray))
+#    count1+=1
+#    print(j)
 
-for j in range(3, 22):
-    current = subdividedwaveheightarray[j]
-    np.random.shuffle(current)
-    current = current[0:173]
-
-    for k in range(len(current)):
-        finalwaveheightarray.append(current[k])
-    #print(np.size(finalwaveheightarray)-176*(j-3))
-    count1+=1
-    #print(j)
-
-print(count1)
-print('The dataset will have ' + str(len(finalwaveheightarray)) + ' number of pixel images before subdivision')
-count2=0
+#print(count1)
+#print('The dataset will have ' + str(len(finalwaveheightarray)) + ' number of pixel images before subdivision')
+#K = np.histogram(finalwaveheightarray, bins=bins)
+#print(K[0])
+#count2=0
 #for i in lista:
 #    q = i.split('.tif')[0]
 #    p = q.split('_')
-#    waveheight = int(10000000 * float(p[5]))
+#    try:
+#        waveheight = int(10000000 * float(p[8]))
+#    except Exception as e:
+#        print(str(e))
+#
 #    if waveheight in finalwaveheightarray:
-#        filename = 'D:\Sar_download' + '\' + str(i)
-#        shutil.copy2(filename, 'D:\Sar_download3')
+#        filename = 'D:\CNN_storage\s1_output' + '\\' + str(i)
+#        shutil.copy2(filename, 'D:\CNN_storage\Balanced_dataset_sep_2021_mini')
 #        count2+=1
+#        print('hej')
 
-print(count2)
+#print(count2)
 print('klar')
 
 plt.hist(waveheightarray, bins = bins)
 plt.title("histogram")
 plt.show()
+#plt.hist(finalwaveheightarray, bins = bins)
+#plt.title("histogram")
+#plt.show()
